22cf770db8820b05947c2406600bafd223d01d5f
shreyansh <shreyansh@gmail.com>
2025-09-05 10:24:17 +0530

    Refactor ImageResponse interface to use ImageType for images

--- utils/types.ts
+++ utils/types.ts
@@ -9,16 +9,6 @@ export interface ImageType {
     updated_at: string;
 }
 export interface ImageResponse {
-    images: Array<{
-        id: string;
-        name: string;
-        description: string;
-        thumbnail_url: string;
-        image_url: string;
-        category: string;
-        created_at: string;
-        updated_at: string;
-      [key: string]: any;
-    }>;
-    [key: string]: any;
-  }
+  images: ImageType[];
+  [key: string]: any;
+}
